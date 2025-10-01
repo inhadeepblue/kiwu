a1c27117811c14799fb14a3c93231a5474d2a9e1
nodeNayn <janaj2143119@naver.com>
2025-10-01 23:47:43 +0900

    Edit line 8

--- week05.py
+++ week05.py
@@ -1,6 +1,11 @@
 import requests
 
-url = f"https://wttr.in/jeju?format=%C+%t"
+
+# # 나라 이름 치고 옵션설정 
+# url = f"https://wttr.in/jeju?format=%C+%t"
+# 도시랑 아이콘 
+# https://github.com/chubin/wttr.in
+url = f"https://wttr.in/Sapporo?format=3" 
 # url = f"https://kin.naver.com/park"
 # url = f"https://wttr.in/incheon?&n&Q"
 # url = f"https://wttr.in/incheon?&0&Q"
