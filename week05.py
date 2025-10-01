212799babe73271cdc7dccdf1e3eef7cd3266567
Eunhyun Kim <kesther4669@naver.com>
2025-10-01 23:04:22 +0900

    update week05.py

--- week05.py
+++ week05.py
@@ -1,13 +1,24 @@
 import requests
 
-url = f"https://wttr.in/jeju?format=%C+%t"
+city = "Daegu"
+url = f"https://wttr.in/{city}?format=%C+%t"
 # url = f"https://kin.naver.com/park"
 # url = f"https://wttr.in/incheon?&n&Q"
 # url = f"https://wttr.in/incheon?&0&Q"
+
+# response = requests.get(url)
+# print(response)
+# print(response.status_code)
+
+# if response.status_code == 200:
+#     print(response.text.strip())
+# else:
+#     print(f"상태 코드 : {response.status_code}")
+
 response = requests.get(url)
-print(response)
-print(response.status_code)
+
 if response.status_code == 200:
-    print(response.text.strip())
+    weather = response.text.strip()
+    print(f"{city}의 날씨는 {weather} 입니다.")
 else:
-    print(f"상태 코드 : {response.status_code}")
+    print(f"날씨 정보를 가져오지 못했습니다. 상태 코드: {response.status_code}")
